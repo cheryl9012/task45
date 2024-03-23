b9b3c84333c116e4b21981c49f43c381432a99f3
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-23 22:00:46 +0500

    Add files via upload

---
+++ Q45.js
@@ -0,0 +1,11 @@
+"use strict";
+// Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+Object.defineProperty(exports, "__esModule", { value: true });
+function createcar(manufacture, model, optional) {
+    return Object.assign({ manufacture,
+        model }, optional);
+}
+let mycar = createcar("Honda", "Civic", { color: "Black", year: "2024" });
+console.log(createcar("Toyota", "Corolla", { color: "Red", year: "2020" }));
+console.log(createcar("Ford", " Fiesta", { color: "White", "sunroof": true }));
+console.log(mycar);
